65f6a26fe9a52c92e07ef3a0e877e0be8297011a
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-26 04:22:47 +0100

    calcul de la duree entre les 2 dates

--- compte.js
+++ compte.js
@@ -3,4 +3,7 @@ const maintenant = new Date()
 
 //Date de fin du compteur
 const noel = new Date("2023-12-25T00:00:00")
-console.log(noel)
+
+//Durée entre les 2 dates
+const duree = noel - maintenant
+console.log(duree)
