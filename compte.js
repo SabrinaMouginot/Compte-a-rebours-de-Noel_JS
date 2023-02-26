cf1ca9f78979647d0a55d81275557a0f2c54e4bb
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-26 05:15:36 +0100

    calcul nb de jours et d heures restants

--- compte.js
+++ compte.js
@@ -10,7 +10,19 @@ const duree = noel - maintenant
 // Calcul du nombre de jour dans la durée
 // durée d'un jour : 24h * 60min * 60s * 1000ms
 // 86 400 000 ms
-
 let jours = Math.floor(duree / ( 24 * 60 * 1000 ))
 
-console.log(jours)
+// on calcule le temps restant après retrait des jours
+// let reste = duree - (jours * 86400000)
+let reste = duree % ( 24 * 60 * 60 * 1000 )
+// % est le modulo (il donne le reste des divisions euclidiennes)
+
+// Calcul du nombre d'heures dans le "reste"
+// Durée d'une heure : 60 min * 60 s * 1000 ms
+// 3 600 000 ms
+let heures = Math.floor(reste / (60*60*1000))
+//Math.floor pour obtenir un entier
+
+// On calcule le temps restant après retrait des heures 
+reste = reste % (60* 60 * 1000)
+console.log(reste)
