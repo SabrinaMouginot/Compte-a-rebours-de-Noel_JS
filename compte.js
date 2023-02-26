a0b1fd370063090c3c949e4cb30e7ebebe229025
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-26 04:32:28 +0100

    commentaire sur le calcul du nb de jr ds la duree en ms

--- compte.js
+++ compte.js
@@ -6,4 +6,8 @@ const noel = new Date("2023-12-25T00:00:00")
 
 //Durée entre les 2 dates
 const duree = noel - maintenant
+
+// Calcul du nombre de jour dans la durée
+// durée d'un jour : 24h * 60min * 60s * 1000ms
+// 86 400 000 ms
 console.log(duree)
